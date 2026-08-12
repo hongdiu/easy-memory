da8831f77ad7425c91f1253b8ca473b9ffb21656
15180618815 <1335822121@qq.com>
2026-08-12 20:33:16 +0800

    fix: global_query_page 缺少 foundation import 导致 kIsWeb 未定义

--- lib/pages/global_query_page.dart
+++ lib/pages/global_query_page.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:easy_memory/models/match_item.dart';
